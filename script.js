5a6d512ff718cb7b3d33f76821a805077cad4f7a
SatoShinya553 <139861110+SatoShinya553@users.noreply.github.com>
2024-10-16 15:37:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,253 +1,89 @@
-/* 目次のスタイル */
-.toc-001 {
-    margin-bottom: 30px;
-    padding: 16px 16px 16px 32px;
-    border: 1px solid #999;
-    background-color: #f7f7f7;
-    color: #333333;
+let pyodideReady = false;
+let pyodide;
+
+async function loadPyodideAndPackages() {
+    try {
+        console.log("Loading Pyodide...");
+        pyodide = await loadPyodide();
+        pyodideReady = true;
+        console.log("Pyodide loaded");
+    } catch (error) {
+        console.error("Failed to load Pyodide", error);
+    }
+    await pyodide.loadPackage("numpy");
+    await pyodide.loadPackage("scikit-learn");
+    await pyodide.loadPackage("pandas");
 }
 
-.toc-001 div {
-    display: flex;
-    align-items: center;
-    margin: 0;
-    padding: 5px 0;
-}
+loadPyodideAndPackages();
 
-.toc-001 ol {
-    list-style-type: decimal;
-    margin: 0;
-    padding: 0 19.2px;
-    overflow: hidden;
-}
+document.querySelector('.run-btn').addEventListener('click', async function() {
+    if (!pyodideReady) {
+        alert("Pyodide is still loading. Please try again in a moment.");
+        return;
+    }
 
-.toc-001 ol ol {
-    margin-top: 5px;
-}
+    const inputId = this.getAttribute('data-input-id');
+    const outputId = this.getAttribute('data-output-id');
+    const output = document.getElementById(outputId);
+    let code = document.getElementById(inputId).value;
 
-.toc-001 li {
-    padding: 5px 0;
-}
+    // 改行文字を正しく処理
+    code = code.replace(/\\n/g, '\n');
 
-.toc-001 a {
-    color: #166c9d;
-}
+    try {
+        console.log("Running Python code...");
 
+        // 標準出力をキャプチャする設定
+        await pyodide.runPythonAsync(`
+            import sys
+            from io import StringIO
 
-/* 見出しのスタイル */
-.heading-41 {
-    display: flex;
-    align-items: center;
-    padding: 8px 11.2px;
-    background-color: #f2f2f2;
-    color: #333333;
-}
+            # 標準出力をキャプチャするための新しい StringIO オブジェクトを作成
+            sys.stdout = StringIO()
+        `);
 
-.heading-41::before {
-    display: inline-block;
-    width: 5px;
-    height: 24px;
-    margin-right: 8px;
-    background-color: #346e9d;
-    content: '';
-}
+        // Pythonコードの実行
+        await pyodide.runPythonAsync(code);
 
-/*実行ボタン*/
-.run-btn {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-    width: 250px;
-    margin:0 auto;
-    padding: .9em 2em;
-    overflow: hidden;
-    border: 1px solid #28a745;
-    border-radius: 5px;
-    background-color: #fff;
-    color: #28a745;
-    font-size: 1em;
-}
+        // キャプチャした標準出力の内容を取得
+        const result = await pyodide.runPythonAsync('sys.stdout.getvalue()');
 
-.run-btn:hover {
-    background-color: transparent;
-    color: #fff;
-}
+        console.log("Python code executed");
+        output.innerHTML = result ? `<pre>${result}</pre>` : '<pre>Code executed successfully, but no output</pre>';
+    } catch (error) {
+        console.error("Error running Python code:", error);
+        output.innerHTML = `<pre style="color: red;">${error}</pre>`;
+    }
+});
 
-.run-btn::before {
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: -1;
-    width: 0;
-    height: 100%;
-    background-color: #28a745;
-    content: '';
-    transition: width .3s ease;
-}
-
-.run-btn:hover::before {
-    width: 100%;
-}
-
-/*リセットボタン*/
-.reset-btn {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-    width: 250px;
-    margin:0 auto;
-    padding: .9em 2em;
-    overflow: hidden;
-    border: 1px solid #ff0000;
-    border-radius: 5px;
-    background-color: #fff;
-    color: #ff0000;
-    font-size: 1em;
-}
-
-.reset-btn:hover {
-    background-color: transparent;
-    color: #fff;
-}
-
-.reset-btn::before {
-    position: absolute;
-    top: 0;
-    right: 0;
-    z-index: -1;
-    width: 0;
-    height: 100%;
-    background-color: #ff0000;
-    content: '';
-    transition: width .3s ease;
-}
-
-.reset-btn:hover::before {
-    width: 100%;
-}
-
-/*アコーディオンメニュー*/
-.accordion-001 {
-    max-width: 500px;
-    background-color: #f5f5f5;
-}
-
-.accordion-001:not([open]) {
-    margin-bottom: 7px;
-}
-
-.accordion-001 summary {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    position: relative;
-    padding: 1em 2em;
-    border-radius: 5px;
-    background-color: #346e9d;
-    color: #fff;
-    font-weight: 600;
-    cursor: pointer;
-}
-
-.accordion-001 summary::-webkit-details-marker {
-    display: none;
-}
-
-.accordion-001 summary::after {
-    transform: translateY(-25%) rotate(45deg);
-    width: 7px;
-    height: 7px;
-    margin-left: 10px;
-    border-bottom: 3px solid #fff;
-    border-right: 3px solid #fff;
-    content: '';
-    transition: transform .3s;
-}
-
-.accordion-001[open] summary::after {
-    transform: rotate(225deg);
-}
-
-.accordion-001 p {
-    transform: translateY(-10px);
-    opacity: 0;
-    margin: 0;
-    padding: 1em 2em 2em 2em;
-    color: #333333;
-    transition: transform .5s, opacity .5s;
-}
-
-.accordion-001[open] p {
-    transform: none;
-    opacity: 1;
-}
-
-/*ボックス*/
-.box-012 {
-    max-width: 400px;
-    margin: 0;
-    border: 2px solid #346e9d;
-    border-radius: 3px;
-    overflow: hidden;
-}
-
-.box-012 span {
-    padding: .4em .8em;
-    background-color: #346e9d;
-    color: #fff;
-}
-
-.box-012 p {
-    margin: 0;
-    padding: 1em 1.5em;
-    color: #333;
-}
-
-/*問題*/
-.heading-24 {
-    position: relative;
-    padding-top: 1.5em;
-    color: #333333;
-    font-size: 20px;
-}
-
-.heading-24::before {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    z-index: -1;
-    color: #d9f4ff;
-    font-size: 40px;
-    line-height: 1;
-    content: attr(data-number);
-    pointer-events: none;
-}
-
-/*例*/
-.box-021 {
-    position: relative;
-    max-width: 400px;
-    margin: 1em auto;
-    padding: 1em 1.5em;
-    border: 2px solid #346e9d;
-    border-radius: 3px;
-    color: #333;
-}
-
-.box-021::before {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: absolute;
-    top: -1em;
-    left: -1em;
-    width: 2em;
-    height: 2em;
-    border-radius: 50%;
-    background-color: #346e9d;
-    color: #fff;
-    font-weight: 600;
-    content: attr(data-number);
-}
+document.querySelector('.reset-btn').addEventListener('click', function() {
+    const inputId = this.getAttribute('data-input-id');
+    document.getElementById(inputId).value = '';
+});
+const TAB_STR = '  '
+document.addEventListener('keydown', e => {
+  if (e.target.tagName !== 'TEXTAREA' || e.keyCode !== 9) return false
+  e.preventDefault()
+  const slct = { left: e.target.selectionStart, right: e.target.selectionEnd }
+  const lineStart = e.target.value.substr(0, slct.left).split('\n').length - 1
+  const lineEnd = e.target.value.substr(0, slct.right).split('\n').length - 1
+  const lines = e.target.value.split('\n')
+  for (const i in lines) {
+    if (i < lineStart || i > lineEnd || lines[i] === '') continue
+    if (!e.shiftKey) {
+      // 行頭にタブ挿入
+      lines[i] = TAB_STR + lines[i]
+      slct.left += i == lineStart ? TAB_STR.length : 0
+      slct.right += TAB_STR.length
+    } else if (lines[i].substr(0, TAB_STR.length) === TAB_STR) {
+      // 行頭のタブ削除
+      lines[i] = lines[i].substr(TAB_STR.length)
+      slct.left -= i == lineStart ? TAB_STR.length : 0
+      slct.right -= TAB_STR.length
+    }
+  }
+  e.target.value = lines.join('\n')
+  e.target.setSelectionRange(slct.left, slct.right)
+  return false
+});
